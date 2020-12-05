bc5ee39932d2eecbdeb6607debaa4693bf1817e0
IJumpAround <>
2020-12-04 23:47:19 -0500

    day 4 update

--- revised_solutions/day4b.py
+++ revised_solutions/day4b.py
@@ -1,24 +1,35 @@
 import marshmallow
 from marshmallow import ValidationError
-from marshmallow.fields import String, Number, Integer
+from marshmallow.fields import String, Number
 from marshmallow.validate import Range, Regexp, OneOf
 
 
+def validate_hgt(value):
+    if 'cm' in value and (150 <= int(value[:-2]) <= 193):
+        return True
+    elif 'in' in value and (59 <= int(value[:-2]) <= 76):
+        return True
+    else:
+        raise ValidationError("Not correct")
+
+def number_len_validator(value):
+    if len(str(value)) != 9:
+        raise ValidationError("bad")
+
 class MySchema(marshmallow.Schema):
-    #
     byr = Number(required=True,validate=Range(min=1920,max=2002))
     iyr = Number(required=True,validate=Range(min=2010,max=2020))
     eyr = Number(required=True,validate=Range(min=2020,max=2030))
-    hgt = String(required=True)
+    hgt = String(required=True, validate=validate_hgt)
     hcl = String(required=True,validate=Regexp('#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]'))
     ecl  = String(required=True,validate=OneOf(['amb','blu','brn','gry','grn','hzl','oth']))
-    pid = Integer(required=True)
+    pid = String(required=True, validate=number_len_validator)
     cid = String(required=False)
 
-lines = []
+schema = MySchema()
+
 batches = []
-batch = []
-with open('puzzle_input/day4/input.txt') as f:
+with open('../puzzle_input/day4/input.txt') as f:
     batch = []
     for line in f:
         if line.strip() == "":
@@ -27,52 +38,26 @@ with open('puzzle_input/day4/input.txt') as f:
         else:
             batch += line.strip().split(' ')
 batches.append(batch)
-# req = {'hgt':,'hcl','ecl','pid','byr':[1920,2002],'iyr','eyr'}
-opt = ['cid']
 
 valid = 0
 for batch in batches:
     d = {}
-    nope=False
     for cmd in batch:
         l = cmd.split(':')
         cmd = l[0]
         val = l[1]
         d[cmd]=val
 
-    # for r in req:
-    #     if r not in d:
-    #         nope=True
-    #         break
-    #     nope=False
-
-    schema = MySchema()
-
     try:
-
-        assert len(d['pid']) == 9
         schema.load(d)
-
-        hgt = d['hgt']
-        print(f'{hgt=}')
-        if 'cm' in hgt:
-            num = hgt[:-2]
-            print(f'{num=}')
-            if 150 <= int(num) <= 193:
-                valid += 1
-        elif 'in' in hgt:
-            num = hgt[:-2]
-            if 59 <= int(num) <= 76:
-                valid +=1
+        valid += 1
     except ValidationError as e:
-        print(e.messages)
-        print(d)
-        print()
+        pass
     except Exception as e:
         print(e)
-        print(0)
 
+print(valid)
+assert valid == 167
+print('Success!')
 
-
-print(f'{valid=}')
 
